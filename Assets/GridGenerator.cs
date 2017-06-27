ba921ff03c02970fa51a5cecb165a6a768a184a5
SAE\bHoughton <benhoughta@hotmail.com>
2017-06-27 22:31:58 +1000

    Switched the axis lables around to actually be X and Z

--- Assets/GridGenerator.cs
+++ Assets/GridGenerator.cs
@@ -6,26 +6,24 @@ public class GridGenerator : MonoBehaviour {
 
     public Transform gridSquare;
     public int maxXValue;
-    public int maxYValue;
-    private int currentYValue = 1;
+    public int maxZValue;
+    private int currentZValue = 1;
 
 
     // Use this for initialization
     void Start () {
         {
-            for (int i = 0; i < maxXValue;)
+            for (int i = 0; i < maxZValue;)
             {
-                Instantiate(gridSquare, new Vector3(currentYValue * 1.1f, 0, i * 1.1f), Quaternion.identity);
+                Instantiate(gridSquare, new Vector3(currentZValue * 1.1f, 0, i * 1.1f), Quaternion.identity);
                 i++;
-                if ((i == (maxXValue)) & (currentYValue != maxYValue))
+                if ((i == (maxZValue)) & (currentZValue != maxXValue))
                 {
                     i = 0;
-                    currentYValue++;
+                    currentZValue++;
                 }
             }
-     
         }
-    
     }
 	
 	// Update is called once per frame
