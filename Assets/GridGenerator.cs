791f43cb4353957e3be1115c219f01e6fd82621e
SAE\bHoughton <benhoughta@hotmail.com>
2017-06-27 18:34:23 +1000

    Created a grid generator script that can be resized depending on 2 variables (variables changed in the editor).

---
+++ Assets/GridGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridGenerator : MonoBehaviour {
+
+    public Transform gridSquare;
+    public int maxXValue;
+    public int maxYValue;
+    private int currentYValue = 1;
+
+
+    // Use this for initialization
+    void Start () {
+        {
+            for (int i = 0; i < maxXValue;)
+            {
+                Instantiate(gridSquare, new Vector3(currentYValue * 1.1f, 0, i * 1.1f), Quaternion.identity);
+                i++;
+                if ((i == (maxXValue)) & (currentYValue != maxYValue))
+                {
+                    i = 0;
+                    currentYValue++;
+                }
+            }
+     
+        }
+    
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
