be04c53b76594aae9270cc9f616aa0c65076d909
SAE\bHoughton <benhoughta@hotmail.com>
2017-06-28 19:11:57 +1000

    Made the code more efficient when setting up the grid. Creates an array and stores the tiles. There is now and end and a start point that can be changed.

--- Assets/Scripts/GridGenerator.cs
+++ Assets/Scripts/GridGenerator.cs
@@ -2,46 +2,70 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GridGenerator : MonoBehaviour {
+public class GridGenerator : MonoBehaviour
+{
 
-    public Transform gridSquare;
-    public Transform StartGridSquare;
-    public Transform FinishGridSquare;
+    public GameObject gridSquare;
+    public GameObject StartGridSquare;
+    public GameObject FinishGridSquare;
     public int maxXValue;
     public int maxZValue;
     private int currentXValue = 1;
-
+    GameObject[,] gridSquares;
+    public int StartX = 0;
+    public int StartY = 0;
+    public int EndX = 9;
+    public int EndY = 9;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
+
+        gridSquares = new GameObject[maxXValue, maxZValue];
+
+
+        for (int i = 0; i < maxZValue; i++)
         {
-            for (int i = 0; i < maxZValue;)
+            for (int j = 0; j < maxXValue; j++)
             {
-                if (i == 0 & currentXValue == 1)
+                if (j == StartX && i == StartY)
                 {
-                    Instantiate(StartGridSquare, new Vector3(currentXValue * 1.1f, 0, i * 1.1f), Quaternion.identity);
-                    i++;
+                    gridSquares[j, i] = Instantiate(StartGridSquare, new Vector3(j * 1.1f, 0, i * 1.1f), Quaternion.identity);
                 }
-
-                Instantiate(gridSquare, new Vector3(currentXValue * 1.1f, 0, i * 1.1f), Quaternion.identity);
-                i++;
-
-                if ((i == (maxZValue - 1)) & currentXValue == maxXValue)
+                else if (j == EndX && i == EndY)
                 {
-                    Instantiate(FinishGridSquare, new Vector3(currentXValue * 1.1f, 0, i * 1.1f), Quaternion.identity);
-                    i++;
+                    gridSquares[j, i] = Instantiate(FinishGridSquare, new Vector3(j * 1.1f, 0, i * 1.1f), Quaternion.identity);
                 }
-                if ((i == (maxZValue)) & (currentXValue != maxXValue))
+                else
                 {
-                    i = 0;
-                    currentXValue++;
+                    gridSquares[j, i] = Instantiate(gridSquare, new Vector3(j * 1.1f, 0, i * 1.1f), Quaternion.identity);
                 }
             }
+            /*      
+             *      if (i == 0 & currentXValue == 1)
+                  {
+                      Instantiate(StartGridSquare, new Vector3(currentXValue * 1.1f, 0, i * 1.1f), Quaternion.identity);
+                      i++;
+                  }
+
+                  if ((i == (maxZValue - 1)) & currentXValue == maxXValue)
+                  {
+                      Instantiate(FinishGridSquare, new Vector3(currentXValue * 1.1f, 0, i * 1.1f), Quaternion.identity);
+                      i++;
+                  }
+                  if ((i == (maxZValue)) & (currentXValue != maxXValue))
+                  {
+                      i = 0;
+                      currentXValue++;
+                  }
+              */
         }
+
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
