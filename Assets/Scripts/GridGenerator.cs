846b1f7d474b44fc72be66d9a790d5abf38a21ac
SAE\bHoughton <benhoughta@hotmail.com>
2017-06-28 14:05:19 +1000

    Changed the code to place a start and end point where the grid ends and begins. Added 2 prefabs: Start and Finish Space prefabs for the grid.

---
+++ Assets/Scripts/GridGenerator.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridGenerator : MonoBehaviour {
+
+    public Transform gridSquare;
+    public Transform StartGridSquare;
+    public Transform FinishGridSquare;
+    public int maxXValue;
+    public int maxZValue;
+    private int currentXValue = 1;
+
+
+    // Use this for initialization
+    void Start () {
+        {
+            for (int i = 0; i < maxZValue;)
+            {
+                if (i == 0 & currentXValue == 1)
+                {
+                    Instantiate(StartGridSquare, new Vector3(currentXValue * 1.1f, 0, i * 1.1f), Quaternion.identity);
+                    i++;
+                }
+
+                Instantiate(gridSquare, new Vector3(currentXValue * 1.1f, 0, i * 1.1f), Quaternion.identity);
+                i++;
+
+                if ((i == (maxZValue - 1)) & currentXValue == maxXValue)
+                {
+                    Instantiate(FinishGridSquare, new Vector3(currentXValue * 1.1f, 0, i * 1.1f), Quaternion.identity);
+                    i++;
+                }
+                if ((i == (maxZValue)) & (currentXValue != maxXValue))
+                {
+                    i = 0;
+                    currentXValue++;
+                }
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
